0c466ab07049f7a590948bf8a8c4520df8f01b5d
Andressa <dhee.andressa@gmail.com>
2025-09-20 04:03:09 -0300

    testando um CRUD completo

--- Tests_web/resources/libs/database.py
+++ Tests_web/resources/libs/database.py
@@ -1,9 +1,20 @@
 from robot.api.deco import keyword
 from pymongo import MongoClient
-
+import bcrypt
 
 Client = MongoClient('mongodb+srv://andressa:dhee.14@mark85.agzdykw.mongodb.net/?retryWrites=true&w=majority&appName=mark85')
 db = Client['test']
+
+@keyword ('limpar user do banco de dados')
+def clean_user(user_email):
+        users =db['users']
+        tasks = db['tasks']
+        us= users.find_one({'email':user_email})
+        
+        if (us):
+                tasks.delete_many({'user':us['_id']})
+                users.delete_many({'email': user_email})
+
 
 @keyword ('remover usuario do banco de dados')
 def remove_user(email):
@@ -12,12 +23,13 @@ def remove_user(email):
         print('removendo o email '+ email)
 @keyword ('inserir usuario no banco de dados')
 def insert_user(user):
-        # doc = {
-        #         'name': name,
-        #         'email': email,
-        #         'password': password
-        # }
+        hash_pass= bcrypt.hashpw(user['password'].encode('utf-8'), bcrypt.gensalt(8))
+        doc = {
+                 'name': user['name'],
+                 'email': user['email'],
+                 'password': hash_pass
+         }
         users = db['users']
-        users.insert_one(user)
+        users.insert_one(doc)
         print(user)
        
--- Tests_web/tests/login.robot
+++ Tests_web/tests/login.robot
@@ -1,5 +1,6 @@
 *** Settings ***
 Documentation    cenarios de atenticação
+Library    Collections
 
 Resource    ../resources/base.resource
 
@@ -18,6 +19,24 @@ CT07:logar com usuario cadastrado
     ...    name=tester      
     ...    email=test@mail.com    
     ...    password=senha123 
+    
+    remover usuario do banco de dados     ${user}[email]
+    inserir usuario no banco de dados    ${user}
+
     submeter formulario de login    ${user}
     conferencia de login    ${user}[name]
-    Sleep    1
+
+CT08:não logar com senha invalida
+
+    ${user}    Create Dictionary     
+    ...    name=tester02      
+    ...    email=test02@mail.com    
+    ...    password=senha1234 
+    
+    remover usuario do banco de dados     ${user}[email]
+    inserir usuario no banco de dados    ${user}
+
+    Set To Dictionary    ${user}    password=senhaincorreta    
+
+    submeter formulario de login    ${user}
+    conferir erro de login    Ocorreu um erro ao fazer login, verifique suas credenciais.
